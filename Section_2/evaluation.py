be004fe7e9cbc10ae6fc658da5f9d7417ea1d9f3
SMahdiHosseini <smhadihosseini4@gmail.com>
2021-12-12 13:30:27 +0330

    average of messages for each topology calculated and appropriate figures added! part 2 completed

--- Section_2/evaluation.py
+++ Section_2/evaluation.py
@@ -1,4 +1,23 @@
 import pathlib
+import matplotlib.pyplot as plt
+import numpy as np
+plt.style.use('ggplot')
+plt.rcParams['figure.figsize'] = (15, 10)
+plt.rcParams.update({
+    "lines.color": "black",
+    "patch.edgecolor": "black",
+    "text.color": "black",
+    "axes.facecolor": "white",
+    "axes.edgecolor": "black",
+    "axes.labelcolor": "black",
+    "xtick.color": "black",
+    "ytick.color": "black",
+    "grid.color": "gray",
+    "figure.facecolor": "white",
+    "figure.edgecolor": "white",
+    "savefig.facecolor": "white",
+    "savefig.edgecolor": "white",
+    "font.size": 25})
 
 complete_data_dict = dict([(i, []) for i in range(5, 50, 5)])
 path_data_dict = dict([(i, []) for i in range(5, 50, 5)])
@@ -36,14 +55,69 @@ def add_new_data(logs_directory):
         others_data_dict[int(file_cat[1])].append(total_messsages)
 
 
+def construct_plot(data, name):
+    plt.plot([i for i in range(5, 50, 5)], [np.average(data[key]) for key in data],
+             label=name, linewidth=4)
+
+    plt.xlabel("number of nodes(network size)")
+    plt.ylabel("average number of messages")
+    # plt.show()
+
+
+def show_all_in_one():
+    construct_plot(complete_data_dict, "complete graph")
+    construct_plot(path_data_dict, "path graph")
+    construct_plot(ring_data_dict, "ring graph")
+    construct_plot(star_data_dict, "star graph")
+    plt.legend()
+    plt.show()
+
+
+def show_individual():
+    construct_plot(complete_data_dict, "complete graph")
+    plt.title("complete graph message analyse")
+    plt.show()
+
+    construct_plot(path_data_dict, "path graph")
+    plt.title("path graph message analyse")
+    plt.show()
+
+    construct_plot(ring_data_dict, "ring graph")
+    plt.title("ring graph message analyse")
+    plt.show()
+
+    construct_plot(star_data_dict, "star graph")
+    plt.title("star graph message analyse")
+    plt.show()
+
+
+def save_data_into_file():
+    file = open("./results/results.txt", 'w')
+
+    file.write("complete graph: ")
+    file.write(str(complete_data_dict) + "\n")
+
+    file.write("path graph: ")
+    file.write(str(path_data_dict) + "\n")
+
+    file.write("ring graph: ")
+    file.write(str(ring_data_dict) + "\n")
+
+    file.write("star graph: ")
+    file.write(str(star_data_dict) + "\n")
+
+    file.write("input graphs: ")
+    file.write(str(others_data_dict) + "\n")
+
+
 # message calculation
 output_dir = "output/inputs"
 for directory in pathlib.Path(output_dir).glob('*'):
     add_new_data(str(directory) + "/logs")
 
+# showing data
+show_all_in_one()
+show_individual()
 
-print(complete_data_dict)
-print(path_data_dict)
-print(ring_data_dict)
-print(star_data_dict)
-print(others_data_dict)
+# save to file
+save_data_into_file()
